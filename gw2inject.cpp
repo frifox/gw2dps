b524781b66749a504c00fc6f4be8ad5b527fe6e6
Daniel Stroh <daniel.stroh@student.kit.edu>
2015-06-06 10:45:06 +0200

    remove unnecessary code

--- gw2inject.cpp
+++ gw2inject.cpp
@@ -1,4 +1,4 @@
-#include <afxwin.h>
+#include <windows.h>
 
 #include <tlhelp32.h> 
 #include <shlwapi.h> 
@@ -15,41 +15,11 @@
 BOOL Inject(DWORD pID, const char * DLL_NAME);
 DWORD GetTargetThreadIDFromProcName(const char * ProcName);
 
-#define CONFIG_DIR_ENV_KEY L"GW2DPS_CONFIG_DIR"
-
-
-// The one and only application object
-
-CWinApp theApp;
-
 using namespace std;
 
-int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+int main(int argc, TCHAR* argv[], TCHAR* envp[])
 {
 	int nRetCode = 0;
-
-	HMODULE hModule = ::GetModuleHandle(NULL);
-
-	if (hModule != NULL)
-	{
-		// initialize MFC and print and error on failure
-		if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
-		{
-			// TODO: change error code to suit your needs
-			_tprintf(_T("Fatal Error: MFC initialization failed\n"));
-			nRetCode = 1;
-		}
-		else
-		{
-			// TODO: code your application's behavior here.
-		}
-	}
-	else
-	{
-		// TODO: change error code to suit your needs
-		_tprintf(_T("Fatal Error: GetModuleHandle failed\n"));
-		nRetCode = 1;
-	}
 
 	// Retrieve process ID 
 	DWORD pID = GetTargetThreadIDFromProcName("Guild Wars 2");
