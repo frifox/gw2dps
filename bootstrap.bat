04210ab76ba0b5ce841e9aa03dad5896ffc071df
Daniel Stroh <daniel.stroh@student.kit.edu>
2015-06-07 19:51:19 +0200

    fix bootstrap (still throws an error, but it works)

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,8 +1,7 @@
+@echo off
 cd ..
 git clone https://bitbucket.org/rafzi/hacklib.git
-mv gw2dps hacklib/src
 cd hacklib/src
 git clone https://bitbucket.org/rafzi/hacklib_gw2.git
-cd ..
-cmake .
-rm src/gw2dps/bootstrap.bat
+cd ../..
+mv "gw2dps" "hacklib/src"
